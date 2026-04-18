9f63350e0dd84d271f3bd3f7021abb57a98dfb48
Andreas P. <25672097+Aypee95@users.noreply.github.com>
2026-04-18 19:42:00 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+## Setup
+
+```bash
+git clone <repo>
+cd github_analyzer
+python -m venv venv
+source venv/bin/activate  # or .\venv\bin\Activate.ps1 on Windows
+pip install -r requirements.txt
+python app.py
+```
+
+Create the OAUTH App: https://github.com/settings/developers
+-> Add client id and secret to .env variables "GITHUB_CLIENT_ID" and "GITHUB_CLIENT_SECRET"
+
+Generate a secret key for Flask to use:
+```python
+import secrets
+print(secrets.token_urlsafe(32))
+```
+-> Add output to .env variable "FLASK_SECRET_KEY"
+
+See .env.example for all env variables used in this project.
+
+## Dependencies
+Flask
+requests
+python-dotenv
---
+++ app.py
@@ -0,0 +1,74 @@
+from flask import Flask, render_template, request, redirect, session
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+GITHUB_CLIENT_ID = os.getenv("GITHUB_CLIENT_ID")
+GITHUB_CLIENT_SECRET = os.getenv("GITHUB_CLIENT_SECRET")
+
+app = Flask(__name__)
+app.secret_key = os.getenv("FLASK_SECRET_KEY")
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/login")
+def login():
+    github_auth_url = (
+        "https://github.com/login/oauth/authorize"
+        f"?client_id={GITHUB_CLIENT_ID}&scope=repo"
+    )
+    
+    return redirect(github_auth_url)
+
+@app.route("/callback")
+def callback():
+    code = request.args.get("code")
+
+    token_url = "https://github.com/login/oauth/access_token"
+    
+    response = requests.post(token_url, data={
+        "client_id": GITHUB_CLIENT_ID,
+        "client_secret": GITHUB_CLIENT_SECRET,
+        "code": code
+    }, headers={"Accept": "application/json"})
+
+    token = response.json()["access_token"]
+    session["github_token"] = token
+    
+    return redirect("/dashboard")
+
+@app.route("/dashboard")
+def dashboard():
+    token = session.get("github_token")
+
+    if not token:
+        return redirect("/")
+
+    headers = {
+        "Authorization": f"token {token}"
+    }
+
+    user_res = requests.get("https://api.github.com/user", headers=headers)
+    repos_res = requests.get("https://api.github.com/user/repos", headers=headers)
+
+    user = user_res.json()
+    repos = repos_res.json()
+
+    return render_template("dashboard.html", user=user, repos=repos)
+
+@app.route("/profile", methods=["POST"])
+def profile():
+    username = request.form["username"]
+    
+    url = f"https://api.github.com/users/{username}"
+    response = requests.get(url)
+    data = response.json()
+
+    return f"User: {data['login']}, Public repos: {data['public_repos']}"
+
+if __name__ == "__main__":
+    app.run(debug=True)
