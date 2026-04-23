d2e64682e91352c35c3a3ac63691983737678e76
Andreas P. <25672097+Aypee95@users.noreply.github.com>
2026-04-23 22:30:12 +0200

    refactor

--- app/github/activity.py
+++ app/github/activity.py
@@ -1,7 +1,5 @@
 import requests
 from .constans import *
-import plotly.graph_objects as go
-from collections import Counter
 
 def get_user_events(token, user):
     headers = {
@@ -14,35 +12,3 @@ def get_user_events(token, user):
     print(res.json())
     
     return res.json()
-
-
-def pushes_per_day(events):
-    push_counts = {}
-    for event in events:
-        if event["type"] == "PushEvent":
-            date = event["created_at"][:10]
-            if date not in push_counts:
-                push_counts[date] = 0
-            push_counts[date] += 1
-
-    fig = go.Figure(data=[
-        go.Bar(x=list(push_counts.keys()), y=list(push_counts.values()))
-    ])
-    fig.update_layout(title="Pushes per Day", xaxis_title="Date", yaxis_title="Pushes")
-   
-    return fig.to_html(full_html=False)
-
-
-def event_type_breakdown(events):
-    type_counts = {}
-    for event in events:
-        event_type = event["type"]
-        if event_type not in type_counts:
-            type_counts[event_type] = 0
-        type_counts[event_type] += 1
-
-    fig = go.Figure(data=[
-        go.Pie(labels=list(type_counts.keys()), values=list(type_counts.values()))
-    ])
-    fig.update_layout(title="Event Types")
-    return fig.to_html(full_html=False)
--- app/routes/activity_routes.py
+++ app/routes/activity_routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, redirect, session
 
 from ..github.activity import *
+from ..github.charts import *
 from ..github.user import *
 
 activity_bp = Blueprint("activity", __name__)
